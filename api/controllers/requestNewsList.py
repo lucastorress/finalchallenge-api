2119665cd46575ef1b11339ac75f6970d45f4a3d
Lucas P Torres de Araujo <lucast0rr3s@gmail.com>
2019-09-25 20:38:51 -0300

    Added func of request latest news

--- api/controllers/requestNewsList.py
+++ api/controllers/requestNewsList.py
@@ -8,29 +8,5 @@ api = CryptoControlAPI(API_AUTH)
 # Connect to a self-hosted proxy server (to improve performance) that points to cryptocontrol.io
 proxyApi = CryptoControlAPI(API_AUTH, "http://cryptocontrol_proxy/api/v1/public")
 
-# Enable the sentiment datapoints
-api.enableSentiment()
-
-# Get top news
-print(api.getTopNews())
-
-# get latest russian news
-print(api.getLatestNews("po"))
-
-# get top bitcoin news
-print(api.getTopNewsByCoin("bitcoin"))
-
-# get top EOS tweets
-print(api.getTopTweetsByCoin("eos"))
-
-# get top Ripple reddit posts
-print(api.getLatestRedditPostsByCoin("ripple"))
-
-# get reddit/tweets/articles in a single combined feed for NEO
-print(api.getTopFeedByCoin("neo"))
-
-# get latest reddit/tweets/articles (seperated) for Litecoin
-print(api.getLatestItemsByCoin("litecoin"))
-
-# get details (subreddits, twitter handles, description, links) for ethereum
-print(api.getCoinDetails("ethereum"))
+def latest_news(lang="po"):
+    return api.getLatestNews(lang)
