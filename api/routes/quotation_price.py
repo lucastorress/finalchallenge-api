27eb3430d6583f3fa65c1f4be75370aff9211a66
Lucas P Torres de Araujo <lucast0rr3s@gmail.com>
2019-09-26 16:39:04 -0300

    Ranklist endpoint with real data update

--- api/routes/quotation_price.py
+++ api/routes/quotation_price.py
@@ -2,9 +2,10 @@ from flask import jsonify
 from api import app
 
 import logging as logger
-from api.controllers import responseListOfCoins as request_quotation_price
+# from api.controllers import responseListOfCoins as request_quotation_price
+from api.controllers import requestListOfCoins as request_quotation_price
 
-response = request_quotation_price.RESPONSE
+response = request_quotation_price.list_of_coins()
 
 
 @app.route('/api/v1/ranklist', methods=['GET'])
